1844f06ba3a3526cd79e52e8c3e218f9bb2554a7
Alessio Buti <alessiobuti@Mac.station>
2025-10-31 19:30:07 +0100

    Aggiornata interfaccia con Bootstrap e aggiunta elimina prodotti

--- app.py
+++ app.py
@@ -2,80 +2,68 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-# Magazzino e ordini
-magazzino = {}  # nome_prodotto: quantità
-ordini = {}     # id_ordine: {'prodotti': [(nome, quantita, prezzo)], 'totale': valore_totale}
-
-ordine_id_counter = 1  # Contatore per identificare gli ordini
+# Magazzino e kit
+prodotti = {}
+kit = {}
 
 @app.route('/')
 def index():
-    return render_template('index.html', magazzino=magazzino, ordini=ordini)
+    return render_template('index.html', prodotti=prodotti, kit=kit)
 
-# Aggiungi prodotto al magazzino
+# Aggiungi prodotto
 @app.route('/add_product', methods=['POST'])
 def add_product():
     nome = request.form['nome_prodotto']
     quantita = int(request.form['quantita_prodotto'])
-    if nome in magazzino:
-        magazzino[nome] += quantita
+    if nome in prodotti:
+        prodotti[nome] += quantita
     else:
-        magazzino[nome] = quantita
+        prodotti[nome] = quantita
     return redirect(url_for('index'))
 
 # Modifica quantità prodotto
 @app.route('/update_product/<nome>', methods=['POST'])
 def update_product(nome):
     nuova_quantita = int(request.form['nuova_quantita'])
-    magazzino[nome] = nuova_quantita
+    prodotti[nome] = nuova_quantita
     return redirect(url_for('index'))
 
-# Elimina prodotto dal magazzino
+# Elimina prodotto
 @app.route('/delete_product/<nome>')
 def delete_product(nome):
-    if nome in magazzino:
-        del magazzino[nome]
+    if nome in prodotti:
+        del prodotti[nome]
     return redirect(url_for('index'))
 
-# Aggiungi ordine
-@app.route('/add_order', methods=['POST'])
-def add_order():
-    global ordine_id_counter
-    prodotti_selezionati = request.form.getlist('prodotti')
-    quantita_selezionate = request.form.getlist('quantita')
-    prezzi_selezionati = request.form.getlist('prezzo')
+# Aggiungi kit
+@app.route('/add_kit', methods=['POST'])
+def add_kit():
+    nome_persona = request.form['nome_persona']
+    quantita_kit = int(request.form['quantita_kit'])
+    prodotti_inclusi = request.form.getlist('prodotti_kit')
 
-    prodotti_ordine = []
-    totale = 0
+    # Controllo disponibilità
+    for p in prodotti_inclusi:
+        if p not in prodotti or prodotti[p] < quantita_kit:
+            return f"Prodotto {p} non disponibile in quantità sufficiente."
 
-    # Controllo disponibilità e aggiorno magazzino
-    for i, nome in enumerate(prodotti_selezionati):
-        quantita = int(quantita_selezionate[i])
-        prezzo = float(prezzi_selezionati[i])
-        if nome not in magazzino or magazzino[nome] < quantita:
-            return f"Prodotto {nome} non disponibile in quantità sufficiente."
-        magazzino[nome] -= quantita
-        prodotti_ordine.append((nome, quantita, prezzo))
-        totale += quantita * prezzo
+    # Aggiorna magazzino
+    for p in prodotti_inclusi:
+        prodotti[p] -= quantita_kit
 
-    ordini[ordine_id_counter] = {'prodotti': prodotti_ordine, 'totale': totale}
-    ordine_id_counter += 1
+    # Salva kit
+    kit[nome_persona] = {'prodotti': prodotti_inclusi, 'quantita': quantita_kit}
     return redirect(url_for('index'))
 
-# Elimina ordine
-@app.route('/delete_order/<int:ordine_id>')
-def delete_order(ordine_id):
-    if ordine_id in ordini:
-        # Restituisci quantità al magazzino
-        for nome, quantita, _ in ordini[ordine_id]['prodotti']:
-            if nome in magazzino:
-                magazzino[nome] += quantita
-            else:
-                magazzino[nome] = quantita
-        del ordini[ordine_id]
+# Elimina kit
+@app.route('/delete_kit/<nome_persona>')
+def delete_kit(nome_persona):
+    if nome_persona in kit:
+        # Restituisci prodotti al magazzino
+        for p in kit[nome_persona]['prodotti']:
+            prodotti[p] += kit[nome_persona]['quantita']
+        del kit[nome_persona]
     return redirect(url_for('index'))
 
-if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
