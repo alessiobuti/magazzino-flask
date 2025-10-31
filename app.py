c226ad8a245fcd34630ba1bc33c2235215e67438
Alessio Buti <alessiobuti@Mac.station>
2025-10-31 19:19:19 +0100

    Aggiornata interfaccia con Bootstrap e aggiunta elimina prodotti

--- app.py
+++ app.py
@@ -3,77 +3,76 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 # Magazzino e ordini
-prodotti = {}  # {'Miele': 20, 'Burro cacao': 15}
-ordini = {}    # {'Alberto': [{'prodotto': 'Miele', 'quantita': 2, 'prezzo': 5.0}, ...]}
+magazzino = {}  # nome_prodotto: quantità
+ordini = {}     # id_ordine: {'prodotti': [(nome, quantita, prezzo)], 'totale': valore_totale}
+
+ordine_id_counter = 1  # Contatore per identificare gli ordini
 
 @app.route('/')
 def index():
-    # Calcolo valore totale degli ordini
-    totale_valore = 0
-    for ord_list in ordini.values():
-        for item in ord_list:
-            totale_valore += item['quantita'] * item['prezzo']
-    return render_template('index.html', prodotti=prodotti, ordini=ordini, totale_valore=totale_valore)
+    return render_template('index.html', magazzino=magazzino, ordini=ordini)
 
 # Aggiungi prodotto al magazzino
 @app.route('/add_product', methods=['POST'])
 def add_product():
     nome = request.form['nome_prodotto']
     quantita = int(request.form['quantita_prodotto'])
-    if nome in prodotti:
-        prodotti[nome] += quantita
+    if nome in magazzino:
+        magazzino[nome] += quantita
     else:
-        prodotti[nome] = quantita
+        magazzino[nome] = quantita
     return redirect(url_for('index'))
 
-# Modifica quantità prodotto nel magazzino
+# Modifica quantità prodotto
 @app.route('/update_product/<nome>', methods=['POST'])
 def update_product(nome):
     nuova_quantita = int(request.form['nuova_quantita'])
-    prodotti[nome] = nuova_quantita
+    magazzino[nome] = nuova_quantita
     return redirect(url_for('index'))
 
 # Elimina prodotto dal magazzino
 @app.route('/delete_product/<nome>')
 def delete_product(nome):
-    if nome in prodotti:
-        del prodotti[nome]
+    if nome in magazzino:
+        del magazzino[nome]
     return redirect(url_for('index'))
 
-# Aggiungi ordine/kit
+# Aggiungi ordine
 @app.route('/add_order', methods=['POST'])
 def add_order():
-    nome_persona = request.form['nome_persona']
-    prodotti_selezionati = request.form.getlist('prodotti_ordine')
-    quantita_list = request.form.getlist('quantita')
-    prezzi_list = request.form.getlist('prezzo')
+    global ordine_id_counter
+    prodotti_selezionati = request.form.getlist('prodotti')
+    quantita_selezionate = request.form.getlist('quantita')
+    prezzi_selezionati = request.form.getlist('prezzo')
 
-    # Controllo disponibilità
-    for i, p in enumerate(prodotti_selezionati):
-        q = int(quantita_list[i])
-        if p not in prodotti or prodotti[p] < q:
-            return f"Prodotto {p} non disponibile in quantità sufficiente."
+    prodotti_ordine = []
+    totale = 0
 
-    # Aggiorna magazzino e ordini
-    if nome_persona not in ordini:
-        ordini[nome_persona] = []
+    # Controllo disponibilità e aggiorno magazzino
+    for i, nome in enumerate(prodotti_selezionati):
+        quantita = int(quantita_selezionate[i])
+        prezzo = float(prezzi_selezionati[i])
+        if nome not in magazzino or magazzino[nome] < quantita:
+            return f"Prodotto {nome} non disponibile in quantità sufficiente."
+        magazzino[nome] -= quantita
+        prodotti_ordine.append((nome, quantita, prezzo))
+        totale += quantita * prezzo
 
-    for i, p in enumerate(prodotti_selezionati):
-        q = int(quantita_list[i])
-        prezzo = float(prezzi_list[i])
-        prodotti[p] -= q
-        ordini[nome_persona].append({'prodotto': p, 'quantita': q, 'prezzo': prezzo})
-
+    ordini[ordine_id_counter] = {'prodotti': prodotti_ordine, 'totale': totale}
+    ordine_id_counter += 1
     return redirect(url_for('index'))
 
 # Elimina ordine
-@app.route('/delete_order/<nome_persona>')
-def delete_order(nome_persona):
-    if nome_persona in ordini:
-        # Restituisci prodotti al magazzino
-        for item in ordini[nome_persona]:
-            prodotti[item['prodotto']] += item['quantita']
-        del ordini[nome_persona]
+@app.route('/delete_order/<int:ordine_id>')
+def delete_order(ordine_id):
+    if ordine_id in ordini:
+        # Restituisci quantità al magazzino
+        for nome, quantita, _ in ordini[ordine_id]['prodotti']:
+            if nome in magazzino:
+                magazzino[nome] += quantita
+            else:
+                magazzino[nome] = quantita
+        del ordini[ordine_id]
     return redirect(url_for('index'))
 
 if __name__ == "__main__":
